8c4f3424f489bcdcd7f30946dd46de9b3a88faa6
gratipine <gratipine@protonmail.com>
2022-07-03 15:03:58 +0100

    Adding tokens

--- tasks.py
+++ tasks.py
@@ -1,26 +1,28 @@
 import luigi
 import pandas as pd
+import src.algo as algo
+
 
 ## Load data
 class LoadData(luigi.Task):
-    param = luigi.Parameter(42)
+    param = luigi.Parameter(default=42)
 
     def requires(self):
         None
     
-    def run():
-        data = pd.read_feather("data/data_in")
+    def run(self):
+        data = pd.read_csv("data/data_in.csv")
     
     def output(self):
-        return luigi.LocalTarget(f"cache/data_out_{param}")
+        return luigi.LocalTarget(f"cache/data_out_{self.param}")
 
 ## Transform data
 class TransformData(luigi.Task):
-
+    param = luigi.Parameter(42)
     def requires(self):
         return LoadData(self.date)
     
-    def run():
+    def run(self):
         data = pd.read_feather("data/data_in")
     
     def output(self):
@@ -29,11 +31,12 @@ class TransformData(luigi.Task):
 
 ## Train
 class Train(luigi.Task):
-
+    param = luigi.Parameter(42)
+    
     def requires(self):
         return TransformData(self.date)
     
-    def run():
+    def run(self):
         data = pd.read_feather(f"cache/data_tranformed_{param}")
         model = algo.Model()
         model.train(data)
@@ -44,11 +47,12 @@ class Train(luigi.Task):
 
 ## create output
 class Predict(luigi.Task):
+    param = luigi.Parameter(42)
 
     def requires(self):
         return Train(self.date)
     
-    def run():
+    def run(self):
         model = algo.Model()
         model.train()
     
