ba36f17804fda1c921cec829fb5542e67cffb91b
gratipine <gratipine@protonmail.com>
2022-07-03 22:20:02 +0100

    beginning of script ofr notebook

--- scripts/data_in.py
+++ scripts/data_in.py
@@ -6,6 +6,7 @@ import json
 import tweepy as tw
 import src.data_in as dt_in
 import pandas as pd
+import src.data_prep as prep
 from setup import token
 # %%
 tweet_fields = "tweet.fields=text,author_id,created_at"
@@ -16,3 +17,35 @@ out = dt_in.search_twitter(query, tweet_fields, since, token)
 # %%
 out["data"]
 # %%
+out_df, flat = prep.transform_data(out) 
+# %%
+from importlib import reload
+reload(prep)
+# %%
+flat_series = pd.Series(flat)
+# %%
+filler_words = [
+    "the", "to", "of", "have", "in", "a", "is", 
+    "and", "for", "as"]
+
+flat_series = flat_series.str.replace("“", "")
+flat_series = flat_series.str.replace("”", "")
+flat_series = flat_series.str.replace(",", "")
+flat_series = flat_series.str.replace("’s", "")
+
+flat_series = flat_series[
+    ~(flat_series.isin(filler_words))]
+
+flat_series = flat_series[
+    ~(flat_series.str.contains("http"))]
+
+# %%
+from wordcloud import WordCloud, ImageColorGenerator
+import matplotlib.pyplot as plt
+# %%
+# Generate the word cloud from frequencies
+wc = WordCloud().generate_from_text(" ".join(flat_series.values.astype(str)))
+
+plt.imshow(wc)
+plt.axis('off')
+plt.show()
--- src/data_prep.py
+++ src/data_prep.py
@@ -4,7 +4,9 @@ def transform_data(out: dict):
 
     out_df = pd.DataFrame(out["data"])
     out_df["created_date"] = pd.to_datetime(out_df["created_at"])
-    out_df["tokens"] = out_df["text"].str.split()
+    out_df["text_modified"] = out_df["text"].str.lower()
+
+    out_df["tokens"] = out_df["text_modified"].str.split()
     flat_ls = [item for sublist in out_df["tokens"].values for item in sublist]
 
     return out_df, flat_ls
